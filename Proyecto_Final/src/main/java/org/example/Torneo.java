2dae403642c2d19352dd43955fd2cb042dffb18b
Ignacio578 <icontardo2024@udec.cl>
2025-06-30 15:05:16 -0400

    Lógica del torneo

---
+++ Proyecto_Final/src/main/java/org/example/Torneo.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+/*
+Clase donde se gentionara, todo la lógica del torneo y la creación de este
+
+ */
+public class Torneo extends ArrayList {
+    protected String nombreTorneo;
+    protected String fechaInicio;
+    protected String fechaFin;
+
+    protected String nombreJugador;
+    protected String nombreCategoria;
+
+}
